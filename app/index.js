2ec00d18e8ab5437819262581bc223889fc29549
Anirudh <anirudhbs96@gmail.com>
2018-02-22 11:52:57 +0530

    Add states, and spead operator plugin

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
+const CREATE_NOTE = 'CREATE_NOTE'
+const UPDATE_NOTE = 'UPDATE_NOTE'
+
 const initialState = {
   nextNoteId: 1,
   notes: {}
@@ -39,3 +42,49 @@ const renderApp = () => {
 }
 
 renderApp()
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_NOTE: {
+      const id = state.nextNoteId
+      const newNote = {
+        id,
+        content: ''
+      }
+      return {
+        ...state,
+        nextNoteId: id + 1,
+        notes: {
+          ...state.notes,
+          [id]: newNote
+        }
+      }
+    }
+    case UPDATE_NOTE: {
+      const {id, content} = action
+      const editedNote = {
+        ...state.notes[id],
+        content
+      }
+      return {
+        ...state,
+        notes: {
+          ...state.notes,
+          [id]: editedNote
+        }
+      }
+    }
+    default:
+      return state
+  }
+}
+
+const state0 = reducer(undefined, {
+  type: CREATE_NOTE
+})
+
+const state1 = reducer(state0, {
+  type: UPDATE_NOTE,
+  id: 1,
+  content: 'Hello, world! xo'
+})
